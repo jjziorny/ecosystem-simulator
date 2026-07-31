8671db5d70f4a02e2ca121abdfe52800e9a82911
Julia Jeziorny <172399997+jjziorny@users.noreply.github.com>
2026-07-30 23:56:27 -0300

    feat: create empty ecosystem world

--- core/src/main/java/io/github/jjziorny/ecosystem/EcosystemGame.java
+++ core/src/main/java/io/github/jjziorny/ecosystem/EcosystemGame.java
@@ -1,32 +1,12 @@
 package io.github.jjziorny.ecosystem;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class EcosystemGame extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
-
-    @Override
-    public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-    }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
+        ScreenUtils.clear(0.08f, 0.16f, 0.10f, 1f);
     }
 }
